668935e66de1eac116461189f09c02b4c18124c0
Ricardo Carvalho <ricardosc@gmail.com>
2014-04-18 19:12:03 -0300

    Consulta a municipios colocada na rota raiz

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,10 +9,6 @@ angular.module('unbMpcaCswApp', [
   .config(function ($routeProvider, $locationProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'partials/main',
-        controller: 'MainCtrl'
-      })
-      .when('/consultaMunicipio', {
         templateUrl: 'partials/consulta',
         controller: 'MainCtrl'
       })
