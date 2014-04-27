b1a96728e1f9ff2d3b217babf20f0e119a4b7f9d
Marcos César de Oliveira <mcesarhm@gmail.com>
2014-04-27 08:32:23 -0300

    Retira código desnecessário, incluído como exemplo pelo yeoman

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,10 +3,7 @@
 var unbControllers = angular.module('unbMpcaCswApp');
 
 unbControllers.controller('MainCtrl', function ($scope, $http) {
-    $http.get('/api/awesomeThings').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
-  });
+});
 
 unbControllers.controller('FormCtrl', function ($scope, $location, $rootScope) {
     $scope.enviar = function () {
