19ac4dbc51f84370aae68757d5c08e002240fd40
gvanerven <gvanerven@gmail.com>
2014-04-27 12:57:45 -0300

    tratamento de acentos no consultaMunicipio.html

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,10 +11,24 @@ unbControllers.controller('FormCtrl', function ($scope, $location, $rootScope) {
 		$scope.alerta = "Campo Obrigatório";
 	}else{
 		$scope.alerta = "";
-		$rootScope.paramMunicipio = $scope.buscaMunicipio;
+		$rootScope.paramMunicipio = $scope.accentsTidy($scope.buscaMunicipio);
 		$location.path("consultaMunicipiosResultado");
 	}
     };
+    $scope.accentsTidy = function(s){
+            var r=s.toLowerCase();
+            r = r.replace(new RegExp(/[àáâãäå]/g),"a");
+            r = r.replace(new RegExp(/æ/g),"ae");
+            r = r.replace(new RegExp(/ç/g),"c");
+            r = r.replace(new RegExp(/[èéêë]/g),"e");
+            r = r.replace(new RegExp(/[ìíîï]/g),"i");
+            r = r.replace(new RegExp(/ñ/g),"n");                
+            r = r.replace(new RegExp(/[òóôõö]/g),"o");
+            r = r.replace(new RegExp(/œ/g),"oe");
+            r = r.replace(new RegExp(/[ùúûü]/g),"u");
+            r = r.replace(new RegExp(/[ýÿ]/g),"y");
+            return r;
+        };
  });
 
 unbControllers.controller('consultaMunicipiosResultadoCtrl', function ($http, $scope, $rootScope) {
