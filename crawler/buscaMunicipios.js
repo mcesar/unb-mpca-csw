3f068e65bef02103dcd3a2cdcc09c93bd2d1a11c
patriciahma <patriciahma@users.noreply.github.com>
2014-04-27 18:18:11 -0300

    Update buscaMunicipios.js
    Nova versão do script para popular o banco.
    
    https://trello.com/c/lgF6TiAI/31

--- crawler/buscaMunicipios.js
+++ crawler/buscaMunicipios.js
@@ -5,12 +5,19 @@ var request = require('request')
   , format = require('util').format;
 
 mongoose.connect('mongodb://localhost/unb_csw');
+
 var db = mongoose.connection;
+
+var dados = db.collection('municipios');
+dados.remove({},function(err,numRem){
+           console.log('limpa o banco antes de inserir os municipios' + numRem);
+});
 db.on('error', console.error);
 db.once('open', function() {
 	console.log('Conectado ao MongoDB.')
+		
 	var municipio = mongoose.model('municipio', { nome: String, cod: String,  uf: String });
-	
+			
 	var ufs = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO'
 			 , 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI'
 			 , 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO']
@@ -30,30 +37,28 @@ db.once('open', function() {
 				qtde = numpag.substring(buscapag.index + 1, numpag.lenght);
 				for (n=1; n<=qtde; n++){
 					npgs = n.toString();
-					async.eachLimit(npgs, concurrency, function (npg, next) {
-						var url = format('http://portaldatransparencia.gov.br/PortalTransparenciaListaCidades.asp?Exercicio=2013&SelecaoUF=1&SiglaUF=%s&Pagina=%s#',uf, npg.toString());
-						request(url, function (err, response, body) {
-							if (err)
-								throw err;
-							var $ = cheerio.load(body);
-							$('a[href^="PortalTransparenciaListaAcoes.asp"]').each(function () {
-								var href = $(this).attr('href');
-								var nome = ($(this).text()).trim();	
-								var busca = href.match(/CodMun/);
-								var cod = href.substring(busca.index + 7, busca.index + 11);
-								var entrada = new municipio({ nome: nome, cod: cod, uf: uf});
-								entrada.save(function(err){
-									if(err) 
-										throw err;
-									console.log('SALVO com sucesso: %s - %s - %s', uf, nome, cod);
-								});							
-							});
-							next();
+					var url = format('http://portaldatransparencia.gov.br/PortalTransparenciaListaCidades.asp?Exercicio=2013&SelecaoUF=1&SiglaUF=%s&Pagina=%s#',uf, npgs.toString());
+					request(url, function (err, response, body) {
+						if (err)
+							throw err;
+						var $ = cheerio.load(body);
+						$('a[href^="PortalTransparenciaListaAcoes.asp"]').each(function () {
+							var href = $(this).attr('href');
+							var nome = ($(this).text()).trim();	
+							var busca = href.match(/CodMun/);
+							var cod = href.substring(busca.index + 7, busca.index + 11);
+							var entrada = new municipio({ nome: nome, cod: cod, uf: uf});
+							entrada.save(function(err){
+								if(err) 
+									throw err;
+								console.log('SALVO com sucesso: %s - %s - %s', uf, nome, cod);
+							});							
 						});
+						next();
 					});
 				}
-				});
-			next();
+			});
+		    next();
 		});
 	});
 });
