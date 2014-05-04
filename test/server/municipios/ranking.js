21775ae64f1f2c74435f7c20093628be3a1d546e
rnpeclat <rnpeclat@gmail.com>
2014-05-04 19:45:19 -0300

    Ordenação de IDH por UF Refatoração dos controladores Criação de filtro, concentrando a validação já usada.

--- test/server/municipios/ranking.js
+++ test/server/municipios/ranking.js
@@ -19,8 +19,8 @@ describe('GET /api/municipios/ranking/idh', function() {
                 if (err) return done(err);
                 res.body.should.be.instanceof(Array);
                 if(res.body[0].nome == 'ALCANTARAS' &&
-                		res.body[1].cod == '3521' &&
-                		res.body[2].uf == 'AC')
+                		res.body[1].cod == '1371' &&
+                		res.body[2].uf == 'CE')
                 		//res.body[0]._id == '5353fd784d2bc7ed64289af7' &&
                 		//res.body[0].__v == 0)
                 	console.log('Conteúdo testado com sucesso');
@@ -37,8 +37,8 @@ describe('GET /api/municipios/ranking/idh', function() {
                 if (err) return done(err);
                 res.body.should.be.instanceof(Array);
                 if(res.body[0].nome == 'ALCANTARAS' &&
-                		res.body[1].cod == '3521' &&
-                		res.body[2].uf == 'AC')
+                		res.body[1].cod == '1371' &&
+                		res.body[2].uf == 'CE')
                 		//res.body[0]._id == '5353fd784d2bc7ed64289af7' &&
                 		//res.body[0].__v == 0)
                 	console.log('Conteúdo testado com sucesso');
@@ -55,8 +55,8 @@ describe('GET /api/municipios/ranking/idh', function() {
                 if (err) return done(err);
                 res.body.should.be.instanceof(Array);
                 if(res.body[8183].nome == 'ALCANTARAS' &&
-                		res.body[8182].cod == '3521' &&
-                		res.body[8181].uf == 'AC')
+                		res.body[8182].cod == '1371' &&
+                		res.body[8181].uf == 'CE')
                 		//res.body[0]._id == '5353fd784d2bc7ed64289af7' &&
                 		//res.body[0].__v == 0)
                 	console.log('Conteúdo testado com sucesso');
@@ -73,6 +73,71 @@ describe('GET /api/municipios/ranking/idh', function() {
                     done();
                 });
      
+        })
+        
+        it('Se o UF for passado junto com IDH, deve retornar a lista decrescente de todos os municípios daquela UF ordenados por idh', function(done) {
+        request(app)
+            .get('/api/municipios/ranking/idh?ordem=&uf=CE')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end(function(err, res) {
+                if (err) return done(err);
+                res.body.should.be.instanceof(Array);
+                if(res.body[0].nome == 'ALCANTARAS' &&
+                		res.body[1].cod == '1371' &&
+                		res.body[2].idh == 0.284)
+                		//res.body[0]._id == '5353fd784d2bc7ed64289af7' &&
+                		//res.body[0].__v == 0)
+                	console.log('Conteúdo testado com sucesso');
+                done();
+            });
     })
+    
+    it('Se o UF for passado junto com IDH e ORDEM = desc, deve retornar a lista decrescente de todos os municípios daquela UF ordenados por idh', function(done) {
+        request(app)
+            .get('/api/municipios/ranking/idh?ordem=desc&uf=CE')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end(function(err, res) {
+                if (err) return done(err);
+                res.body.should.be.instanceof(Array);
+                if(res.body[0].nome == 'ALCANTARAS' &&
+                		res.body[1].cod == '1371' &&
+                		res.body[2].idh == 0.284)
+                		//res.body[0]._id == '5353fd784d2bc7ed64289af7' &&
+                		//res.body[0].__v == 0)
+                	console.log('Conteúdo testado com sucesso');
+                done();
+            });
+    })
+    
+    it('Se o UF for passado junto com IDH e ordem = asc, deve retornar a lista crescente de todos os municípios daquela UF ordenados por idh', function(done) {
+        request(app)
+            .get('/api/municipios/ranking/idh?ordem=asc&uf=CE')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end(function(err, res) {
+                if (err) return done(err);
+                res.body.should.be.instanceof(Array);
+                if(res.body[239].nome == 'ALCANTARAS' &&
+                		res.body[238].cod == '1371' &&
+                		res.body[237].idh == 0.284)
+                		//res.body[0]._id == '5353fd784d2bc7ed64289af7' &&
+                		//res.body[0].__v == 0)
+                	console.log('Conteúdo testado com sucesso');
+                done();
+            });
+    })
+    
+     it('Se o parâmetro uf for diferente do padrão [a-zA-Z][a-zA-Z], deve retornar 400', function(done) {
+            request(app)
+                .get('/api/municipios/ranking/idh?ordem=asc1&uf=<script>')
+                .expect(400)
+                .end(function(err, res) {
+                    if (err) return done(err);
+                    done();
+                });
+     
+        })
     
 });
